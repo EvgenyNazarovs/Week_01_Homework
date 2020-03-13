8a168604dc511b715512f781314509825f07b381
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-03-13 16:48:46 +0000

    solves add pet to customer test

--- starting_point/pet_shop.rb
+++ starting_point/pet_shop.rb
@@ -64,3 +64,11 @@ end
 def customer_pet_count(customer)
   return customer[:pets].length
 end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets] << new_pet
+end
+
+def customer_can_afford_pet(customer, new_pet)
+
+end 
