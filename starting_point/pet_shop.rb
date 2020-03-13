4dfeee22f048819f90fe9ecd30b0f38f677e5799
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-03-13 16:37:30 +0000

    solves test 12

--- starting_point/pet_shop.rb
+++ starting_point/pet_shop.rb
@@ -38,5 +38,13 @@ def find_pet_by_name(pet_shop, name)
       return pet
     end
   end
-  return nil 
+  return nil
+end
+
+def remove_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet_name == pet[:name]
+      pet_shop[:pets].delete(pet)
+    end
+  end
 end
